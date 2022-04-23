23f9e080cdf21a5b46cf24b93502b21e8bed4739
omidsotooni <mr.sotooni@yahoo.com>
2022-04-23 23:00:17 +0430

    Adding Template files and Addnig EntitiesUser files

---
+++ ShopWithASP.NETCore.Doima/Entities/Users/Role.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopWithASP.NETCore.Doima.Entities.Users
+{
+    public class Role
+    {
+        public long  RoleId { get; set; }
+        public string Name { get; set; }
+        public ICollection<UserInRole> UserInRoles { get; set; }
+    }
+}
---
+++ ShopWithASP.NETCore.Doima/Entities/Users/UserInRole.cs
@@ -0,0 +1,11 @@
+﻿namespace ShopWithASP.NETCore.Doima.Entities.Users
+{
+    public class UserInRole
+    {
+        public long UserInRoleId { get; set; }
+        public virtual User User { get; set; }
+        public long UserId { get; set; }
+        public virtual Role Role { get; set; }
+        public long RoleId { get; set; }
+    }
+}
