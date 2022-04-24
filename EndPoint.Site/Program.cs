899df501f91c1421f732eb13d556992e93d1932c
omidsotooni <mr.sotooni@yahoo.com>
2022-04-25 00:54:06 +0430

    some changes

--- EndPoint.Site/Program.cs
+++ EndPoint.Site/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add framework services [UseNpgsql]
 builder.Services.AddScoped<IDataBaseContext, DataBaseContext>();
 //{ "DefaultConnection": "Host = ; Port = 5432; Username = ; Password = ; Database = Users; SSL Mode = Require"
-//"Host=172.16.44.230;Port=5432;Database=sadra;Username=omid;Password=12345678"
+//"Host=127.0.0.1;Port=5432;Database=store;Username=omid;Password=12345678"
 string PostgreSqlConnectionString = @"Host=127.0.0.1;Port=5432;Database=ShopDB;Username=postgres;Password=123456";
 builder.Services.AddEntityFrameworkNpgsql().
     AddDbContext<DataBaseContext>(options => options.UseNpgsql(PostgreSqlConnectionString));
